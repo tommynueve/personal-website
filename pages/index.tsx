0b1af2c702fd21af02ef7253848b69b2557ef5d7
Tomás Andrés  Bangerter <tomas.bangerter@Macbook-248.local>
2021-04-07 18:56:52 -0300

    Initial setup: create-next-app + cleared boiler plate. Also added: sass and typescript.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>My portfolio</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <header>Hello world!</header>
+    </>
+  );
+}
