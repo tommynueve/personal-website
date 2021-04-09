bde12ada20710c594b8ce771854ebdd47c5f320c
tommynueve <totti_b@hotmail.com>
2021-04-09 18:57:16 -0300

    Started Homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HomeSection from '@sections/home';
 import Layout from '@sections/layout';
 import Head from 'next/head';
 
@@ -8,7 +9,9 @@ export default function Home() {
         <title>My portfolio</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Layout>Home Page</Layout>
+      <Layout>
+        <HomeSection />
+      </Layout>
     </>
   );
 }
---
+++ sections/home/index.tsx
@@ -0,0 +1,12 @@
+import style from './Home.module.scss';
+
+const HomeSection = () => {
+  return (
+    <div className={style.homeContainer}>
+      <h1>Tomas Bangerter</h1>
+      <h2>Frontend Developer</h2>
+    </div>
+  );
+};
+
+export default HomeSection;
