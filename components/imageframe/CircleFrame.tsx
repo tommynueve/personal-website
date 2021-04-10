59da838882e76ede5d5b83e304bb7b2f8908068b
tommynueve <totti_b@hotmail.com>
2021-04-10 00:01:41 -0300

    Basic layout for the mobile Home screen without much styling yet.

---
+++ components/imageframe/CircleFrame.tsx
@@ -0,0 +1,10 @@
+import style from './CircleFrame.module.scss';
+
+const CircleFrame = () => {
+  return (
+    <div className={style.container}>
+      <img src='https://via.placeholder.com/150' alt='my picture' />
+    </div>
+  );
+};
+export default CircleFrame;
--- sections/home/index.tsx
+++ sections/home/index.tsx
@@ -1,10 +1,18 @@
+import CircleFrame from '@components/imageframe/CircleFrame';
 import style from './Home.module.scss';
 
 const HomeSection = () => {
   return (
     <div className={style.homeContainer}>
-      <h1>Tomas Bangerter</h1>
-      <h2>Frontend Developer</h2>
+      <CircleFrame />
+      <h1 className={style.title}>Tomas Bangerter</h1>
+      <h2 className={style.subtitle}>Frontend Developer with 3+ years of experience in React</h2>
+      <p>
+        I started in the IT world as a business analyst, earned a position as a Product Owner, and although I was in a
+        path of success, that didn't make me happy... I wanted to actually build apps myself! So one day, I took a big
+        risk, I started over as a web developer and I have been one for 3+ years now. Was it worth it? I start every day
+        with a smile on my face. Best decision I have ever made.
+      </p>
     </div>
   );
 };
