025269b70c7145a6286b06d69b54fdc657700096
Tomás Andrés  Bangerter <tomas.bangerter@Macbook-248.local>
2021-04-19 14:46:04 -0300

    Added favicon. Added author image. Changed circleframe image to Nextjs Image component.

--- components/imageframe/CircleFrame.tsx
+++ components/imageframe/CircleFrame.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+
 import style from './CircleFrame.module.scss';
 
 const CircleFrame = () => {
   return (
     <div className={style.container}>
-      <img src='https://via.placeholder.com/150' alt='my picture' />
+      <Image src='/author.jpeg' alt='my picture' width='150px' height='150px'></Image>
     </div>
   );
 };
